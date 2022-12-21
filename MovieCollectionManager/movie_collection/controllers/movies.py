71e3d9004177bf85fe55ebdce55547498ca059b7
Akshay <ak.rajmane.91@gmail.com>
2022-12-22 02:37:20 +0530

    added movies thirdparty service added get moveis endpoint

---
+++ MovieCollectionManager/movie_collection/controllers/movies.py
@@ -0,0 +1,18 @@
+from rest_framework.decorators import api_view
+
+from movie_collection.thirdparty import movies as movies_service
+from movie_collection.helpers.response_helper import JSONResponse
+
+
+class MoviesController:
+    @staticmethod
+    @api_view(["GET"])
+    def get_movies(request):
+        params = request.GET
+        success, response = movies_service.get_movies(url=params.get("next"))
+        kwargs = {
+            "data": response,
+            "status": 200 if success else 500
+        }
+
+        return JSONResponse(**kwargs)
