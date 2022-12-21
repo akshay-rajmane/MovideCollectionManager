46cf4887250f59725391d2792dbd208a65b18e52
Akshay <ak.rajmane.91@gmail.com>
2022-12-21 23:42:11 +0530

    added customuser to admin site

--- MovieCollectionManager/movie_collection/admin.py
+++ MovieCollectionManager/movie_collection/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from movie_collection.models import CustomUser
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'is_staff', 'is_deleted', 'is_active',)
+    list_filter = ('is_staff', 'is_active', 'is_deleted',)
+    search_fields = ('email', 'username',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
