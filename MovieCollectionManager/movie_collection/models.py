1770b65fc0bb52713fe09f33ee4af954a05e33ef
Akshay <ak.rajmane.91@gmail.com>
2022-12-21 23:05:07 +0530

    add custom user model update settings

--- MovieCollectionManager/movie_collection/models.py
+++ MovieCollectionManager/movie_collection/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.db.models.fields import UUIDField, EmailField, CharField, BooleanField
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    username = CharField(max_length=250, unique=True, db_index=True)
+    email = EmailField(unique=True, primary_key=True, db_index=True)
+    uuid = UUIDField(unique=True, db_index=True)
+    is_deleted = BooleanField(default=False)
+    def __str__(self):
+        return self.username
